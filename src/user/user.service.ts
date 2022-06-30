7843bc522969c5f92eaebe7c89cd4677edae4da9
Max <zeetreex@gmail.com>
2022-06-30 22:00:15 +0300

    wrote put, delete endpoints for admin to update users, also get all users, total users count and pipe for verifying id from mongodb

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common'
 import { InjectModel } from 'nestjs-typegoose'
 import { UserModel } from './user.model'
 import { ModelType } from '@typegoose/typegoose/lib/types'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { genSalt, hash } from 'bcryptjs'
 
 @Injectable()
 export class UserService {
@@ -13,5 +15,55 @@ export class UserService {
     const user = await this.userModel.findById(_id)
     if (!user) throw new NotFoundException('User not found')
     return user
+  }
+
+  async updateProfile(_id: string, dto: UpdateUserDto) {
+    const user = await this.byId(_id)
+    const isSameUser = await this.userModel.findOne({ email: dto.email })
+
+    if (isSameUser && String(_id) !== String(isSameUser._id)) {
+      throw new NotFoundException('Email is taken by another user')
+    }
+
+    if (dto.password) {
+      const salt = await genSalt(10)
+      user.password = await hash(dto.password, salt)
+    }
+
+    user.email = dto.email
+    console.log(dto.email)
+    if (dto.isAdmin || dto.isAdmin === false) {
+      user.isAdmin = dto.isAdmin
+    }
+
+    await user.save()
+
+    return
+  }
+
+  async getCount() {
+    return this.userModel.find().count().exec()
+  }
+
+  async getAllUsers(searchTerm?: string) {
+    let options = {}
+
+    if (searchTerm) {
+      options = {
+        $or: [
+          {
+            email: new RegExp(searchTerm, 'i'),
+          },
+        ],
+      }
+    }
+
+    return this.userModel.find(options).select('-password -updatedAt -__v').sort({
+      createdAt: 'desc',
+    })
+  }
+
+  async deleteUser(id: string) {
+    return this.userModel.findByIdAndDelete(id).exec()
   }
 }
