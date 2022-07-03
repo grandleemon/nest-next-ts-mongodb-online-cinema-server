a71d8cb78861ff5203dd2f06cc6b7e4ea9ab17b1
Max <zeetreex@gmail.com>
2022-07-03 20:03:02 +0300

    finished movie entity, tested it, all endpoints are working

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,4 +1,86 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common'
+import { MovieService } from './movie.service'
+import { IdValidationPipe } from '../pipes/id.validation.pipe'
+import { Types } from 'mongoose'
+import { Auth } from '../auth/decorators/auth.decorator'
+import { UpdateMovieDto } from './dto/update-movie.dto'
 
-@Controller('movie')
-export class MovieController {}
+@Controller('movies')
+export class MovieController {
+
+  constructor(private readonly movieService: MovieService) {
+  }
+
+  @Get('by-slug/:slug')
+  async bySlug(@Param('slug') slug: string) {
+    return this.movieService.bySlug(slug)
+  }
+
+  @Get('by-actor/:actorId')
+  async byActor(@Param('actorId', IdValidationPipe) actorId: Types.ObjectId) {
+    return this.movieService.byActor(actorId)
+  }
+
+  @Post('by-genres')
+  @HttpCode(200)
+  async byGenres(@Body('genresIds') genresIds: Types.ObjectId[]) {
+    return this.movieService.byGenres(genresIds)
+  }
+
+  @Get('most-popular')
+  async getMostPopular() {
+    return this.movieService.getMostPopular()
+  }
+
+  @Get(':id')
+  @Auth('admin')
+  async byId(@Param('id', IdValidationPipe) id: string) {
+    return this.movieService.byId(id)
+  }
+
+  @Get()
+  async getAll(@Query('searchTerm') searchTerm: string) {
+    return this.movieService.getAll(searchTerm)
+  }
+
+  @Put('update-count-opened')
+  @HttpCode(200)
+  async updateCountOpened(@Body('slug') slug: string) {
+    return this.movieService.updateCountOpened(slug)
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post()
+  @HttpCode(200)
+  @Auth('admin')
+  async create() {
+    return this.movieService.create()
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put(':id')
+  @HttpCode(200)
+  @Auth('admin')
+  async update(@Param('id', IdValidationPipe) id: string, @Body() dto: UpdateMovieDto) {
+    return this.movieService.update(id, dto)
+  }
+
+  @Delete(':id')
+  @HttpCode(200)
+  @Auth('admin')
+  async delete(@Param('id', IdValidationPipe) id: string) {
+    return this.movieService.delete(id)
+  }
+}
--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -1,4 +1,116 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { InjectModel } from 'nestjs-typegoose'
+import { MovieModel } from './movie.model'
+import { DocumentType, ModelType } from '@typegoose/typegoose/lib/types'
+import { Types } from 'mongoose'
+import { UpdateMovieDto } from './dto/update-movie.dto'
 
 @Injectable()
-export class MovieService {}
+export class MovieService {
+  constructor(@InjectModel(MovieModel) private readonly movieModel: ModelType<MovieModel>) {
+  }
+
+  async getAll(searchTerm: string) {
+    let options = {}
+
+    if (searchTerm) {
+      options = {
+        $or: [
+          {
+            title: new RegExp(searchTerm, 'i'),
+          },
+        ],
+      }
+    }
+
+    return this.movieModel.find(options)
+      .select('-updatedAt -__v')
+      .sort({ createdAt: 'desc' })
+      .populate('actors genres')
+      .exec()
+  }
+
+  async bySlug(slug: string) {
+    const movie = await this.movieModel.findOne({ slug }).populate('actors genres').exec()
+
+    if (!movie) throw new NotFoundException('Movie was not found')
+
+    return movie
+  }
+
+  async byId(id: string) {
+    const movie = await this.movieModel.findById(id)
+
+    if (!movie) throw new NotFoundException('Movie was not found')
+
+    return movie
+  }
+
+  async byActor(actorId: Types.ObjectId) {
+    const actor = await this.movieModel.find({ actors: actorId })
+    if (!actor) throw new NotFoundException('Movies were not found')
+
+    return actor
+  }
+
+  async byGenres(genresIds: Types.ObjectId[]): Promise<DocumentType<MovieModel>[]> {
+    const genres = await this.movieModel.find({ genres: { $in: genresIds } }).exec()
+    if (!genres) throw new NotFoundException('Movies were not found')
+
+    return genres
+  }
+
+  async getMostPopular() {
+    return this.movieModel.find({ countOpened: { $gt: 0 } }).sort({
+      countOpened: -1,
+    }).populate('genres')
+  }
+
+  async updateCountOpened(slug: string) {
+    const update = await this.movieModel.findOneAndUpdate({ slug }, {
+      $inc: { countOpened: 1 },
+    }, { new: true })
+    if (!update) throw new NotFoundException('Movie was not found')
+
+    return update
+  }
+
+  async create() {
+    const defaultValue: UpdateMovieDto = {
+      bigPoster: '',
+      actors: [],
+      genres: [],
+      poster: '',
+      title: '',
+      videoUrl: '',
+      slug: '',
+    }
+
+    const movie = await this.movieModel.create(defaultValue)
+    return movie._id
+  }
+
+  async update(id: string, dto: UpdateMovieDto) {
+    const updateMovie = await this.movieModel.findByIdAndUpdate(id, dto, {
+      bigPoster: dto.bigPoster,
+      actors: dto.actors,
+      genres: dto.genres,
+      poster: dto.poster,
+      title: dto.title,
+      videoUrl: dto.videoUrl,
+      slug: dto.slug,
+    })
+    if (!updateMovie) throw new NotFoundException('Movie was not found')
+
+    return updateMovie
+  }
+
+  async delete(id: string) {
+    const movie = await this.movieModel.findByIdAndDelete(id)
+    if (!movie) throw new NotFoundException('Movie was not found')
+
+    return movie
+  }
+}
+
+
