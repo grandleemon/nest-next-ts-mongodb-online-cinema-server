85d51d21d679a8d35b5e8ff3ad5fa473c7e4df0c
Max <zeetreex@gmail.com>
2022-07-05 18:35:50 +0300

    fixed telegram bot, now it is sending a poster of movie and my message

--- src/config/telegram.config.ts
+++ src/config/telegram.config.ts
@@ -2,7 +2,7 @@ import { Telegram } from '../telegram/telegram.interface'
 
 export const getTelegramConfig = (): Telegram => {
   return {
-    chatId: '5570878759',
-    token: 'AAFJBxmwPYcTJ6pt2VAWaEgolTHOLCr6x8U',
+    chatId: '1671846466',
+    token: '5570878759:AAFJBxmwPYcTJ6pt2VAWaEgolTHOLCr6x8U',
   }
 }
